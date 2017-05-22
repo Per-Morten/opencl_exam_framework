f07925fb574fb507293e83134f82b421d7c6dc3c
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-05-22 19:59:47 +0200

    ref #1: Move invoke and create_resource to util
    Moved the create and invoke functions to opencl_utility as it is more
    relevant there.

--- opencl_lib/opencl_utility.h
+++ opencl_lib/opencl_utility.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <CL/cl.h>
+#include <cp_lib.h>
 
 namespace cp
 {
@@ -13,5 +14,25 @@ namespace cp
         long int file_size(const char* path);
 
         char* read_program(const char* path);
+
+        ///////////////////////////////////////////////////
+        /// \brief Creates an OpenCL resource, and does
+        ///        error checking.
+        ///        Crashes on failure.
+        ///////////////////////////////////////////////////
+        template<class Create_Func, class... Args>
+        decltype(auto)
+        create_resource(Create_Func func, Args... args);
+
+        ///////////////////////////////////////////////////
+        /// \brief Invokes an OpenCL function and does
+        ///        error checking.
+        ///        Crashes on failure.
+        ///////////////////////////////////////////////////
+        template<class Opencl_Func, class... Args>
+        void
+        invoke(Opencl_Func func, Args... args);
     }
 }
+
+#include <opencl_utility.tpp>
---
+++ opencl_lib/opencl_utility.tpp
@@ -0,0 +1,29 @@
+
+template<class Create_Func, class... Args>
+decltype(auto)
+cp::opencl::create_resource(Create_Func func,
+                            Args... args)
+{
+    cl_int error;
+    auto item = func(args..., &error);
+    if (error != CL_SUCCESS)
+    {
+        CP_ERROR("Could not create resource: %s",
+                 cp::opencl::get_error(error));
+    }
+
+    return item;
+}
+
+template<class Opencl_Func, class... Args>
+void
+cp::opencl::invoke(Opencl_Func func,
+                   Args... args)
+{
+    cl_int error = func(args...);
+    if (error != CL_SUCCESS)
+    {
+        CP_ERROR("Could not execute operation: %s",
+                 cp::opencl::get_error(error));
+    }
+}
